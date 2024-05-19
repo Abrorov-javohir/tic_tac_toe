e653d41857368d4784b35ba1a0b7322040ff107b
Hunter20050910 <145260700+Abrorov-javohir@users.noreply.github.com>
2024-05-20 02:51:01 +0500

    ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,278 +1,335 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TikToe());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isX = true;
-  String box1 = "";
-  String box2 = "";
-  String box3 = "";
-  String box4 = "";
-  String box5 = "";
-  String box6 = "";
-  String box7 = "";
-  String box8 = "";
-  String box9 = "";
+class TikToe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
+      debugShowCheckedModeBanner: false,
+      home: SecondPage(),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  List<String> boxes = ['', '', '', '', '', '', '', '', ''];
+  int counter = 0;
+
+  String result = '';
+
+  void onPressed(int index) {
+    if (boxes[index].isEmpty && result.isEmpty) {
+      boxes[index] = 'X';
+      counter++;
+      result = checkWin();
+      if (counter < 9 && result != 'X') {
+        int randomNumber = Random().nextInt(9);
+        while (boxes[randomNumber].isNotEmpty) {
+          randomNumber = Random().nextInt(9);
+        }
+        boxes[randomNumber] = 'O';
+        counter++;
+        result = checkWin();
+      }
+      setState(() {});
+      if (result.isNotEmpty) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("$result yutdi"),
+              actions: [
+                OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  label: const Text("Restart"),
+                  icon: const Icon(Icons.restart_alt),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+  }
+
+  String checkWin() {
+    if (boxes[0] == "X" && boxes[1] == "X" && boxes[2] == "X") {
+      return "X";
+    } else if (boxes[3] == "X" && boxes[4] == "X" && boxes[5] == "X") {
+      return "X";
+    } else if (boxes[6] == "X" && boxes[7] == "X" && boxes[8] == "X") {
+      return "X";
+    } else if (boxes[0] == "X" && boxes[3] == "X" && boxes[6] == "X") {
+      return "X";
+    } else if (boxes[1] == "X" && boxes[4] == "X" && boxes[7] == "X") {
+      return "X";
+    } else if (boxes[2] == "X" && boxes[5] == "X" && boxes[8] == "X") {
+      return "X";
+    } else if (boxes[0] == "X" && boxes[4] == "X" && boxes[8] == "X") {
+      return "X";
+    } else if (boxes[2] == "X" && boxes[4] == "X" && boxes[6] == "X") {
+      return "X";
+    }
+
+    if (boxes[0] == "O" && boxes[1] == "O" && boxes[2] == "O") {
+      return "O";
+    } else if (boxes[3] == "O" && boxes[4] == "O" && boxes[5] == "O") {
+      return "O";
+    } else if (boxes[6] == "O" && boxes[7] == "O" && boxes[8] == "O") {
+      return "O";
+    } else if (boxes[0] == "O" && boxes[3] == "O" && boxes[6] == "O") {
+      return "O";
+    } else if (boxes[1] == "O" && boxes[4] == "O" && boxes[7] == "O") {
+      return "O";
+    } else if (boxes[2] == "O" && boxes[5] == "O" && boxes[8] == "O") {
+      return "O";
+    } else if (boxes[0] == "O" && boxes[4] == "O" && boxes[8] == "O") {
+      return "O";
+    } else if (boxes[2] == "O" && boxes[4] == "O" && boxes[6] == "O") {
+      return "O";
+    }
+
+    return "";
+  }
+
+  void restart() {
+    boxes = ["", "", "", "", "", "", "", "", ""];
+    counter = 0;
+    result = "";
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Tic-Tac-Toe'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box1 == "") {
-                        setState(() {
-                          box1 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.amber,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box1),
+                InkWell(
+                  onTap: () {
+                    onPressed(0);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[0],
+                      style: TextStyle(fontSize: 50),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box2 == "") {
-                        setState(() {
-                          box2 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.red,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box2),
+                InkWell(
+                  onTap: () {
+                    onPressed(1);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[1],
+                      style: TextStyle(fontSize: 50),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box3 == "") {
-                        setState(() {
-                          box3 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.blue,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box3),
+                InkWell(
+                  onTap: () {
+                    onPressed(2);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[2],
+                      style: TextStyle(fontSize: 50),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box4 == "") {
-                        setState(() {
-                          box4 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.amber,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box4),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box5 == "") {
-                        setState(() {
-                          box5 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.red,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box5),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box6 == "") {
-                        setState(() {
-                          box6 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.blue,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box6),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box7 == "") {
-                        setState(() {
-                          box7 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.amber,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box7),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box8 == "") {
-                        setState(() {
-                          box8 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.red,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box8),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      if (box9 == "") {
-                        setState(() {
-                          box9 = isX ? "X" : "O";
-                          isX = !isX;
-                        });
-                      }
-                    },
-                    child: Container(
-                      color: Colors.blue,
-                      padding: const EdgeInsets.all(20),
-                      child: Text(box9),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Colors.green,
-                  child: TextButton(
-                      onPressed: newGame, child: const Text("New Game")),
                 )
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: double.infinity,
-                  color: Colors.green,
-                  child: TextButton(
-                      onPressed: resetGame, child: const Text("Reset")),
+                InkWell(
+                  onTap: () {
+                    onPressed(3);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[3],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onPressed(4);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[4],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onPressed(5);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[5],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () {
+                    onPressed(6);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[6],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onPressed(7);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[7],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onPressed(8);
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: Text(
+                      boxes[8],
+                      style: TextStyle(fontSize: 50),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    restart();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  child: Text("Reset Game")),
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    restart();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  child: Text("New Game")),
+            ),
           ],
         ),
       ),
     );
-  }
-
-  void newGame() {
-    setState(() {
-      box1 = "";
-      box2 = "";
-      box3 = "";
-      box4 = "";
-      box5 = "";
-      box6 = "";
-      box7 = "";
-      box8 = "";
-      box9 = "";
-      isX = true;
-    });
-  }
-
-  void resetGame() {
-    setState(() {
-      newGame();
-    });
-  }
-
-  String chekWinner() {
-    if (box1 == box2 && box2 == box3 && box1 != "") {
-      return box1;
-    } else if (box4 == box5 && box5 == box6 && box4 != "") {
-      return box4;
-    } else if (box7 == box8 && box8 == box9 && box7 != "") {
-      return box7;
-    } else if (box1 == box4 && box4 == box7 && box1 != "") {
-      return box1;
-    } else if (box2 == box5 && box5 == box8 && box2 != "") {
-      return box2;
-    } else if (box3 == box6 && box6 == box9 && box3 != "") {
-      return box3;
-    } else if (box1 == box5 && box5 == box9 && box1 != "") {
-      return box1;
-    } else if (box3 == box5 && box5 == box7 && box3 != "") {
-      return box3;
-    } else if (box1 != "" &&
-        box2 != "" &&
-        box3 != "" &&
-        box4 != "" &&
-        box5 != "" &&
-        box6 != "" &&
-        box7 != "" &&
-        box8 != "" &&
-        box9 != "") {
-      return "draw";
-    } else {
-      return "no winner";
-    }
   }
 }
